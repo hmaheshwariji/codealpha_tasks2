90d04868ac70bb7d8108319a1bc72843d08d672f
HARSH MAHESHWARI <harshbhaiji4321@gmail.com>
2024-08-20 19:17:47 +0530

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+        name: "bandana",
+        artist: "shubh",
+        img: "music1",
+        src: "music1",
+    },
+    {
+        name: "one love",
+        artist: "shubh",
+        img: "music2",
+        src: "music2",
+    },
+    {
+        name: "no love",
+        artist: "shubh",
+        img: "music3",
+        src: "music3",
+    },
+    {
+        name: "Signs",
+        artist: "guru randhawa",
+        img: "music4",
+        src: "music4",
+    },
+    {
+        name: "cheque",
+        artist: "shubh",
+        img: "music5",
+        src: "music5",
+    },
+]
